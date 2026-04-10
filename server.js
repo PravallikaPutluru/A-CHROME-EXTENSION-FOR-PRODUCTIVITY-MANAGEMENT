0386a291e3471e757046d8bd0ea1bb6163677aa6
PravallikaPutluru <pravallikaputluru@gmail.com>
2026-04-10 16:01:59 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/productivity");
+
+app.use("/api", require("./routes/api"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
